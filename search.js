6790eb5e5b2a1a8918801d4845783fb64d0feb80
skyerobin <115919808+skyerobin@users.noreply.github.com>
2023-11-22 14:20:35 +0900

    Add files via upload

---
+++ search.js
@@ -0,0 +1,36 @@
+const Searching = Search.prototype;
+  
+  function Search(){
+      this.keyword = document.querySelector('input[name = "search"]');
+      this.engine = document.querySelector('.SelectSearch');
+      this.button = document.querySelector('.img-button');
+      this.form = document.querySelector('.search');
+  
+      this.Engine();
+  }
+  
+  Searching.Engine = function(){
+      this.form.addEventListener('submit', e => {
+          
+          e.preventDefault();
+  
+  
+          let engine = this.engine.value;
+          let keyword = this.keyword.value;
+          if(engine === 'google'){
+              location.href = 'https://www.google.co.kr/search?q=' + keyword;
+          }else if(engine === 'naver'){
+              location.href = 'https://search.naver.com/search.naver?query=' + keyword;
+          }else if(engine === 'youtube'){
+            location.href = 'https://www.youtube.com/results?search_query=' + keyword;
+          }else if(engine === 'Navershopping'){
+            location.href = 'https://search.shopping.naver.com/search/all?query=' + keyword;
+          }else if(engine === '시청기록'){
+            location.href ='https://www.youtube.com/feed/history?query=' +keyword;
+          }else{
+            location.href = 'https://twitter.com/search?q=' + keyword;
+          }
+      });
+  }
+  
+  new Search();
