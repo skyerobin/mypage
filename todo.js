1c51fa1812b04374db477f33e208ab0cd51ddb22
skyerobin <115919808+skyerobin@users.noreply.github.com>
2024-06-01 18:25:14 +0900

    Update todo.js

--- todo.js
+++ todo.js
@@ -16,14 +16,14 @@ function deleteToDo(event) {
   saveToDos();
 }
 
-function paintToDo(newTodo) {
-  const li = document.createElement("li");
-  li.id = newTodo.id;
-  const span = document.createElement("span");
+const span = document.createElement("span");
   span.innerText = newTodo.text;
+
   const button = document.createElement("button");
   button.innerText = "❌";
   button.addEventListener("click", deleteToDo);
+
+  li.appendChild(checkbox);
   li.appendChild(span);
   li.appendChild(button);
   toDoList.appendChild(li);
