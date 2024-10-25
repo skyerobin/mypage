d5686cc762ee185952f361d4738deff1eefbb83d
skyerobin <115919808+skyerobin@users.noreply.github.com>
2024-10-25 17:44:58 +0900

    Update search.js

--- search.js
+++ search.js
@@ -1,24 +1,10 @@
-function Search() {
-    this.keyword = document.querySelector('input[name="search"]');
-    this.engine = document.querySelector('.SelectSearch');
-    this.button = document.querySelector('.img-button');
-    this.form = document.querySelector('.search');
-
-    console.log('keyword:', this.keyword);  // 요소가 정상적으로 선택되는지 확인
-    console.log('engine:', this.engine);    // 요소가 정상적으로 선택되는지 확인
-    console.log('button:', this.button);    // 요소가 정상적으로 선택되는지 확인
-    console.log('form:', this.form);        // 요소가 정상적으로 선택되는지 확인
-
-    this.Engine();
-}
-
 Search.prototype.Engine = function() {
     this.form.addEventListener('submit', e => {
         e.preventDefault();
 
         let engine = this.engine.value;
         let keyword = this.keyword.value;
-        
+
         if(engine === 'google'){
             location.href = 'https://www.google.co.kr/search?q=' + keyword;
         } else if(engine === 'naver'){
@@ -29,10 +15,8 @@ Search.prototype.Engine = function() {
             location.href = 'https://search.shopping.naver.com/search/all?query=' + keyword;
         } else if(engine === 'x'){
             location.href = 'https://twitter.com/search?q=' + keyword;
-        } else {
+        } else { // engine 값이 어떤 조건에도 맞지 않는 경우
             location.href = 'https://music.youtube.com/search?q=' + keyword;
         }
     });
 }
-
-new Search();
