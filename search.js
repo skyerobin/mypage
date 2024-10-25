323d28b4d9072c6afb74b27f9ca9e5903e52e6a5
skyerobin <115919808+skyerobin@users.noreply.github.com>
2024-10-25 17:50:11 +0900

    Update search.js

--- search.js
+++ search.js
@@ -7,12 +7,8 @@ Search.prototype.Engine = function() {
 
         if(engine === 'google'){
             location.href = 'https://www.google.co.kr/search?q=' + keyword;
-        } else if(engine === 'naver'){
-            location.href = 'https://search.naver.com/search.naver?query=' + keyword;
         } else if(engine === 'youtube'){
             location.href = 'https://www.youtube.com/results?search_query=' + keyword;
-        } else if(engine === 'Navershopping'){
-            location.href = 'https://search.shopping.naver.com/search/all?query=' + keyword;
         } else if(engine === 'x'){
             location.href = 'https://twitter.com/search?q=' + keyword;
         } else { // engine 값이 어떤 조건에도 맞지 않는 경우
@@ -20,3 +16,4 @@ Search.prototype.Engine = function() {
         }
     });
 }
+new Search()
