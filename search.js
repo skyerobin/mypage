081f624bc69ac64e842937af3f5888512f03e98a
skyerobin <115919808+skyerobin@users.noreply.github.com>
2024-10-25 17:05:49 +0900

    Update search.js

--- search.js
+++ search.js
@@ -11,10 +11,10 @@ Search.prototype.Engine = function() {
             location.href = 'https://search.naver.com/search.naver?query=' + keyword;
         } else if(engine === 'youtube'){
             location.href = 'https://www.youtube.com/results?search_query=' + keyword;
-        } else if(engine === 'Navershopping'){
-            location.href = 'https://search.shopping.naver.com/search/all?query=' + keyword;
         } else if(engine === 'x'){
             location.href = 'https://twitter.com/search?q=' + keyword;
+        } else if(engine === ''){
+            location.href = '';
         } else { // engine 값이 어떤 조건에도 맞지 않는 경우
             location.href = 'https://music.youtube.com/search?q=' + keyword;
         }
