5ede649c31c879ce942decb60bcbfa3b3b15f10b
skyerobin <115919808+skyerobin@users.noreply.github.com>
2024-10-25 15:49:04 +0900

    Update search.js

--- search.js
+++ search.js
@@ -1,35 +1,34 @@
 const Searching = Search.prototype;
-  
-  function Search(){
-      this.keyword = document.querySelector('input[name = "search"]');
-      this.engine = document.querySelector('.SelectSearch');
-      this.button = document.querySelector('.img-button');
-      this.form = document.querySelector('.search');
-  
-      this.Engine();
-  }
-  
-  Searching.Engine = function(){
-      this.form.addEventListener('submit', e => {
-          
-          e.preventDefault();
-  
-  
-          let engine = this.engine.value;
-          let keyword = this.keyword.value;
-          if(engine === 'google'){
-              location.href = 'https://www.google.co.kr/search?q=' + keyword;
-          }else if(engine === 'naver'){
-              location.href = 'https://search.naver.com/search.naver?query=' + keyword;
-          }else if(engine === 'youtube'){
+
+function Search(){
+    this.keyword = document.querySelector('input[name="search"]');
+    this.engine = document.querySelector('.SelectSearch');
+    this.button = document.querySelector('.img-button');
+    this.form = document.querySelector('.search');
+
+    this.Engine();
+}
+
+Search.prototype.Engine = function(){
+    this.form.addEventListener('submit', e => {
+        e.preventDefault();
+
+        let engine = this.engine.value;
+        let keyword = this.keyword.value;
+        if(engine === 'google'){
+            location.href = 'https://www.google.co.kr/search?q=' + keyword;
+        }else if(engine === 'naver'){
+            location.href = 'https://search.naver.com/search.naver?query=' + keyword;
+        }else if(engine === 'youtube'){
             location.href = 'https://www.youtube.com/results?search_query=' + keyword;
-          }else if(engine === 'Navershopping'){
+        }else if(engine === 'Navershopping'){
             location.href = 'https://search.shopping.naver.com/search/all?query=' + keyword;
-          }else if(engine ==='x'){
+        }else if(engine === 'x'){
             location.href = 'https://twitter.com/search?q=' + keyword;
-          } else{
+        } else{
             location.href = 'https://music.youtube.com/search?q=' + keyword;
-      });
-  }
-  
-  new Search();
+        }
+    });
+}
+
+new Search();
