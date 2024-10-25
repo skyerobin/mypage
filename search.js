453727214bc9857b220dc96625f908f39d99d8ea
skyerobin <115919808+skyerobin@users.noreply.github.com>
2024-10-25 17:42:01 +0900

    Update search.js

--- search.js
+++ search.js
@@ -1,29 +1,35 @@
-const Searching = Search.prototype;
-
-function Search(){
+function Search() {
     this.keyword = document.querySelector('input[name="search"]');
     this.engine = document.querySelector('.SelectSearch');
     this.button = document.querySelector('.img-button');
     this.form = document.querySelector('.search');
 
+    console.log('keyword:', this.keyword);  // 요소가 정상적으로 선택되는지 확인
+    console.log('engine:', this.engine);    // 요소가 정상적으로 선택되는지 확인
+    console.log('button:', this.button);    // 요소가 정상적으로 선택되는지 확인
+    console.log('form:', this.form);        // 요소가 정상적으로 선택되는지 확인
+
     this.Engine();
 }
 
-Search.prototype.Engine = function(){
+Search.prototype.Engine = function() {
     this.form.addEventListener('submit', e => {
         e.preventDefault();
 
         let engine = this.engine.value;
         let keyword = this.keyword.value;
+        
         if(engine === 'google'){
             location.href = 'https://www.google.co.kr/search?q=' + keyword;
-        }else if(engine === 'naver'){
+        } else if(engine === 'naver'){
             location.href = 'https://search.naver.com/search.naver?query=' + keyword;
-        }else if(engine === 'youtube'){
+        } else if(engine === 'youtube'){
             location.href = 'https://www.youtube.com/results?search_query=' + keyword;
-        }else if(engine === 'x'){
+        } else if(engine === 'Navershopping'){
+            location.href = 'https://search.shopping.naver.com/search/all?query=' + keyword;
+        } else if(engine === 'x'){
             location.href = 'https://twitter.com/search?q=' + keyword;
-        } else{
+        } else {
             location.href = 'https://music.youtube.com/search?q=' + keyword;
         }
     });
