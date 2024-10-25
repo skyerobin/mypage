6ddf66764249abd93e0747b7cb4d341419bd6160
skyerobin <115919808+skyerobin@users.noreply.github.com>
2024-10-25 17:39:44 +0900

    Update search.js

--- search.js
+++ search.js
@@ -1,22 +1,32 @@
-Search.prototype.Engine = function() {
+const Searching = Search.prototype;
+
+function Search(){
+    this.keyword = document.querySelector('input[name="search"]');
+    this.engine = document.querySelector('.SelectSearch');
+    this.button = document.querySelector('.img-button');
+    this.form = document.querySelector('.search');
+
+    this.Engine();
+}
+
+Search.prototype.Engine = function(){
     this.form.addEventListener('submit', e => {
         e.preventDefault();
 
         let engine = this.engine.value;
         let keyword = this.keyword.value;
-
         if(engine === 'google'){
             location.href = 'https://www.google.co.kr/search?q=' + keyword;
-        } else if(engine === 'naver'){
+        }else if(engine === 'naver'){
             location.href = 'https://search.naver.com/search.naver?query=' + keyword;
-        } else if(engine === 'youtube'){
+        }else if(engine === 'youtube'){
             location.href = 'https://www.youtube.com/results?search_query=' + keyword;
-        } else if(engine === 'x'){
-            location.href = 'https://twitter.com/search?q' + keyword;
-        } else if(engine === ''){
-            location.href = '';
-        } else(engine === 'music') { // engine 값이 어떤 조건에도 맞지 않는 경우
+        }else if(engine === 'x'){
+            location.href = 'https://twitter.com/search?q=' + keyword;
+        } else{
             location.href = 'https://music.youtube.com/search?q=' + keyword;
         }
     });
 }
+
+new Search();
