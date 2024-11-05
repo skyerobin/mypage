b5c16306956c61d8a0ca145d9e36f555525d0dc0
skyerobin <115919808+skyerobin@users.noreply.github.com>
2024-11-05 15:52:59 +0900

    Update music.js

--- music.js
+++ music.js
@@ -2,62 +2,62 @@ const quotes = [
     {
         quote: "I'm finally breathing.",
         author: "Where I belong",
-        songUrl: "https://music.youtube.com/watch?v=Q8GfoU3oRUc&si=NKJ_4rhyJWasAGBl", // 노래 URL 추가
+        songUrl: "https://music.youtube.com/watch?v=Q8GfoU3oRUc&si=NKJ_4rhyJWasAGBl",
     },
     {
         quote: "You cry alone and then he swears he loves you.",
         author: "Face Down",
-        songUrl: "https://music.youtube.com/watch?v=eEpbE2UJXcU&si=IChvWUbPk3zierSw", // 노래 URL 추가
+        songUrl: "https://music.youtube.com/watch?v=eEpbE2UJXcU&si=IChvWUbPk3zierSw",
     },
     {
         quote: "'Cause if you jump I will jump too",
-        author: "Skyway Avenue",     
-        songUrl: "https://music.youtube.com/watch?v=SRGa5kNQqMI&si=ILdyvUVxNQKOwq31", // 노래 URL 추가
+        author: "Skyway Avenue",
+        songUrl: "https://music.youtube.com/watch?v=SRGa5kNQqMI&si=ILdyvUVxNQKOwq31",
     },
     {
         quote: "When you're up at 2AM you know that I'll be up in 5.",
-        author: "tomrrow tonight",     
-        songUrl: "https://music.youtube.com/watch?v=R4wbjdrbDa4&si=U4U3Out0KV7dy2Kw", // 노래 URL 추가
+        author: "tomrrow tonight",
+        songUrl: "https://music.youtube.com/watch?v=R4wbjdrbDa4&si=U4U3Out0KV7dy2Kw",
     },
     {
         quote: "Here's to the ones that we got.",
-        author: "Memories",     
-        songUrl: "https://music.youtube.com/watch?v=lNilb-EaTOU&si=I5VLn2x4M_kQkN2Q", // 노래 URL 추가
+        author: "Memories",
+        songUrl: "https://music.youtube.com/watch?v=lNilb-EaTOU&si=I5VLn2x4M_kQkN2Q",
     },
     {
         quote: "You are my address.",
-        author: "One Way Ticict",     
-        songUrl: "https://music.youtube.com/watch?v=T4RxZyCg6K4&si=FK0SIqWG34C3qC7S", // 노래 URL 추가
+        author: "One Way Ticict",
+        songUrl: "https://music.youtube.com/watch?v=T4RxZyCg6K4&si=FK0SIqWG34C3qC7S",
     },
     {
         quote: "One, two, three, your smile is the best thing in the world.",
-        author: "Hey Song",     
-        songUrl: "https://music.youtube.com/watch?v=T2nmBV59iJ8&si=ZPxb3l4P9K86Bh7D", // 노래 URL 추가
+        author: "Hey Song",
+        songUrl: "https://music.youtube.com/watch?v=T2nmBV59iJ8&si=ZPxb3l4P9K86Bh7D",
     },
     {
         quote: "Let's live like we're immortal.",
-        author: "Wasted Night",     
-        songUrl: "https://music.youtube.com/watch?v=g3kChT7B3Us&si=k4B-X2UrciwSBNB6", // 노래 URL 추가
+        author: "Wasted Night",
+        songUrl: "https://music.youtube.com/watch?v=g3kChT7B3Us&si=k4B-X2UrciwSBNB6",
     },
     {
         quote: "Feel like, feel like Feel like leaving it to the flow.",
-        author: "おもかげ",     
-        songUrl: "https://music.youtube.com/watch?v=jg-uSq64Ru8&si=cmN1UmfG-E7w_uO9", // 노래 URL 추가
+        author: "おもかげ",
+        songUrl: "https://music.youtube.com/watch?v=jg-uSq64Ru8&si=cmN1UmfG-E7w_uO9",
     },
     {
         quote: "最前線に 最前線に 最前線にいこう.",
-        author: "フロントライン",     
-        songUrl: "https://music.youtube.com/watch?v=j4QHBAIWxa8&si=F6u4X-ZubTeEbMAo", // 노래 URL 추가
-    },{
-        quote: "But there never seems to be enough timeTo do the things you want to do once you find them",
-        author: "Time in a Bottle",     
-        songUrl: "https://music.youtube.com/watch?v=gnrSpwb2Ax4&si=KBbGx_SsWDKohAEt", // 노래 URL 추가  
+        author: "フロントライン",
+        songUrl: "https://music.youtube.com/watch?v=j4QHBAIWxa8&si=F6u4X-ZubTeEbMAo",
     },
-    {   
-        quote:"I'm off to a new town to find me a new toyI'm off to a new town to find me a new toy",
-        author:"Strawberry Margarita",
-        songUrl:"https://music.youtube.com/watch?v=uU6TA2T4A5s&si=wACAhpDFmY5vj2Gp",
-        
+    {
+        quote: "But there never seems to be enough time to do the things you want to do once you find them.",
+        author: "Time in a Bottle",
+        songUrl: "https://music.youtube.com/watch?v=gnrSpwb2Ax4&si=KBbGx_SsWDKohAEt",
+    },
+    {
+        quote: "I'm off to a new town to find me a new toy.",
+        author: "Strawberry Margarita",
+        songUrl: "https://music.youtube.com/watch?v=uU6TA2T4A5s&si=wACAhpDFmY5vj2Gp",
     },
 ];
 
@@ -67,10 +67,12 @@ let currentIndex = Math.floor(Math.random() * quotes.length);
 
 // Function to display the current quote
 function displayQuote(index) {
+    // Ensure the index is within bounds
+    if (index < 0 || index >= quotes.length) return;
+
     quoteContainer.innerHTML = `
         <div class="quote" onclick="playMusic(${index})">
             <p>${quotes[index].quote}</p>
-        
         </div>
     `;
 }
