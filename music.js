c2d55fca0d14686b835b1f64fa9aca3db3e361c8
skyerobin <115919808+skyerobin@users.noreply.github.com>
2024-12-05 14:02:37 +0900

    Update music.js

--- music.js
+++ music.js
@@ -2,71 +2,71 @@ const quotes = [
     {
         quote: "'Cause this is where I belong",
         author: "Where I belong",
-        songUrl: "https://music.youtube.com/watch?v=Q8GfoU3oRUc&si=NKJ_4rhyJWasAGBl",
+        songUrl: "https://www.youtube.com/watch?v=Q8GfoU3oRUc&list=PLIY3iPyOhcg7ryefyYx8ofiTVN4c9cK0C",
     },
     {
         quote: "You cry alone and then he swears he loves you.",
         author: "Face Down",
-        songUrl: "https://music.youtube.com/watch?v=eEpbE2UJXcU&si=IChvWUbPk3zierSw",
+        songUrl: "https://www.youtube.com/watch?v=eEpbE2UJXcU&list=PLIY3iPyOhcg7ryefyYx8ofiTVN4c9cK0C&index=2",
     },
     {
-        quote: "'Cause if you jump I will jump too",
+        quote: "'Cause if you jump I will jump to",
         author: "Skyway Avenue",
-        songUrl: "https://music.youtube.com/watch?v=SRGa5kNQqMI&si=ILdyvUVxNQKOwq31",
+        songUrl: "https://www.youtube.com/watch?v=SRGa5kNQqMI&list=PLIY3iPyOhcg7ryefyYx8ofiTVN4c9cK0C&index=3",
     },
     {
         quote: "Find euphoria in dystopia.",
         author: "Dystopia",
-        songUrl: "https://music.youtube.com/watch?v=djkelGOcR-Q&si=CFKsxSgzf3Eye3tn",
+        songUrl: "https://www.youtube.com/watch?v=djkelGOcR-Q&list=PLIY3iPyOhcg7ryefyYx8ofiTVN4c9cK0C&index=4",
     },
     {
         quote: "And though you're dead and gone, believe me.",
         author: "Welcome to the Black Parad",
-        songUrl: "https://music.youtube.com/watch?v=v_uncMEJkBc&si=qhX2flxV4f1D5Dta",
+        songUrl: "https://www.youtube.com/watch?v=v_uncMEJkBc&list=PLIY3iPyOhcg7ryefyYx8ofiTVN4c9cK0C&index=5",
     },
     {
         quote: "You are my address.",
         author: "One Way Ticict",
-        songUrl: "https://music.youtube.com/watch?v=T4RxZyCg6K4&si=xg8g7NzO-yVlPJn8",
+        songUrl: "https://www.youtube.com/watch?v=T4RxZyCg6K4&list=PLIY3iPyOhcg7ryefyYx8ofiTVN4c9cK0C&index=6",
     },
     {
         quote: "I'm not really quite myself.",
         author: "Not Quite Myself",
-        songUrl: "https://music.youtube.com/watch?v=Jhxs4KLPoOA&si=LlMJvcqxTNZVgSsf",
+        songUrl: "https://www.youtube.com/watch?v=Jhxs4KLPoOA&list=PLIY3iPyOhcg7ryefyYx8ofiTVN4c9cK0C&index=7",
     },
     {
         quote: "Let's live like we're immortal.",
         author: "Wasted Night",
-        songUrl: "https://music.youtube.com/watch?v=v=g3kChT7B3Us&si=aQpljDZGKSxjLt_a",
+        songUrl: "https://www.youtube.com/watch?v=g3kChT7B3Us&list=PLIY3iPyOhcg7ryefyYx8ofiTVN4c9cK0C&index=8",
     },
     {
         quote: "Feel like, feel like Feel like leaving it to the flow.",
         author: "おもかげ",
-        songUrl: "https://music.youtube.com/watch?v=jg-uSq64Ru8&si=cmN1UmfG-E7w_uO9",
+        songUrl: "https://www.youtube.com/watch?v=jg-uSq64Ru8&list=PLIY3iPyOhcg7ryefyYx8ofiTVN4c9cK0C&index=9",
     },
     {
         quote: "最前線に 最前線に 最前線にいこう.",
         author: "フロントライン",
-        songUrl: "https://music.youtube.com/watch?v=j4QHBAIWxa8&si=F6u4X-ZubTeEbMAo",
+        songUrl: "https://www.youtube.com/watch?v=j4QHBAIWxa8&list=PLIY3iPyOhcg7ryefyYx8ofiTVN4c9cK0C&index=10",
     },
     {
         quote: "One, two, three, your smile is the best thing in the world.",
         author: "Hey Song",
-        songUrl: "https://music.youtube.com/watch?v=T2nmBV59iJ8&si=ZPxb3l4P9K86Bh7D",
+        songUrl: "https://www.youtube.com/watch?v=T2nmBV59iJ8&list=PLIY3iPyOhcg7ryefyYx8ofiTVN4c9cK0C&index=11",
     },
     {
         quote: "I'm off to a new town to find me a new toy.",
         author: "Strawberry Margarita",
-        songUrl: "https://music.youtube.com/watch?v=uU6TA2T4A5s&si=wACAhpDFmY5vj2Gp",
+        songUrl:"https://www.youtube.com/watch?v=uU6TA2T4A5s&list=PLIY3iPyOhcg7ryefyYx8ofiTVN4c9cK0C&index=12",
     },
     {
         quote: "いつか辿り着くその時まで.",
         author: "君に捧げる応援歌",
-        songUrl: "https://music.youtube.com/watch?v=qDdBiFa82ho&si=UBni2k20LmfC9n0q",
+        songUrl: "https://www.youtube.com/watch?v=qDdBiFa82ho&list=PLIY3iPyOhcg7ryefyYx8ofiTVN4c9cK0C&index=13",
     }
 ];
 
-const defaultSongUrl = "https://music.youtube.com/watch?v=defaultSong";
+const defaultSongUrl = "https://www.youtube.com/watch?v=defaultSong";
 const quoteContainer = document.getElementById("quoteContainer");
 let currentIndex = Math.floor(Math.random() * quotes.length);
 
